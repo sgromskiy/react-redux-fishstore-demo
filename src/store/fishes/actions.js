cffa1a0761e5fe6145917dad236d78e97adb979b
Sergei Gromski <sergei.gromski@gmail.com>
2018-04-06 01:15:55 +0300

    fetch reducer

--- src/store/fishes/actions.js
+++ src/store/fishes/actions.js
@@ -1,35 +1,62 @@
+import fetch from 'cross-fetch';
+
 export const ADD_FISH = 'ADD_FISH';
 export const DELETE_FISH = 'ADD_FISH';
 
 export const FETCH_FISH = 'FETCH_FISH';
 
-
-
 export const addFish = (fish) => {
-    return{
-        type: ADD_FISH,
-        fish
-    }
-}
+	return {
+		type: ADD_FISH,
+		fish
+	};
+};
 
 export const deleteFish = (fishID) => {
-    return {
-        type: DELETE_FISH,
-        fishID
-    }
-}
+	return {
+		type: DELETE_FISH,
+		fishID
+	};
+};
 
-
-export const fetchFish = () => {
-    return {
-        type: DELETE_FISH,
-        status: 'loading'
-    }   
-}
+export const fetchFishRequest = () => {
+	return {
+		type: FETCH_FISH,
+		status: 'request'
+	};
+};
 
 export const fetchFishSuccess = (fishes) => {
-    return {
-        type: DELETE_FISH,
-        fishes
-    }
-}
+	return {
+		type: FETCH_FISH,
+		fishes,
+		status: 'success'
+	};
+};
+
+export const fetchFishError = (error) => {
+	return {
+		type: FETCH_FISH,
+		error,
+		status: 'error'
+	};
+};
+
+export const fetchFish = () => {
+	return function(dispatch) {
+		// start request
+		dispatch(fetchFishRequest());
+
+		//async
+		return fetch('https://fishstore-beeb.restdb.io/fishes', {
+			headers: {
+				'x-apikey': '5ac5e120f0a7555103ceaa15'
+			}
+		})
+			.then((response) => response.json(), (error) => console.log('An error occurred.', error))
+			.then((json) =>
+				// recive data
+				dispatch(fetchFishSuccess(json))
+			);
+	};
+};
--- src/store/fishes/reducer.js
+++ src/store/fishes/reducer.js
@@ -1,11 +1,17 @@
-import { ADD_FISH, DELETE_FISH } from './actions';
-const initialState = []
+import { ADD_FISH, DELETE_FISH, FETCH_FISH } from './actions';
+const initialState = {
+	loadingFishes: false,
+	fishes: []
+};
 
 export const fishes = (state = initialState, action) => {
-    switch(action.type){
-        case  ADD_FISH:
-            return [...state, action.fish];
-        default:
-            return state;
-    }
-}
+	switch (action.type) {
+		case ADD_FISH:
+			return { ...state, fishes: [ ...state.fishes, action.fish ] };
+		case FETCH_FISH:
+			if (action.status === 'request') return { ...state, loadingFishes: true };
+			if (action.status === 'success') return { ...state, fishes: [ ...action.fishes ], loadingFishes: false };
+		default:
+			return state;
+	}
+};
